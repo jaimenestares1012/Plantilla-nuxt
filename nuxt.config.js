cdf379677affab68025e8ecfc1a90de7c97f289b
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-04-01 16:17:54 -0500

    cambios carpetas

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  srcDir: 'src/',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - proyecto-nuevo',
@@ -18,8 +19,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
+  plugins: ['~plugins/store-helpers','~/plugins/vuelidate'],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
@@ -54,7 +54,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
