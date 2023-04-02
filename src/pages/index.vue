676fc643f2e11ac1839373ff9f49bc7c9238a90c
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-04-02 12:56:29 -0500

    se añadio nuevo button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,88 +1,50 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <!-- <NuxtLogo />
-        <VuetifyLogo /> -->
-        <h1>MOTI PETIOTA</h1>
+  <div>
+    <div class="contenedor">
+      <v-card class="d-flex justify-center">
+        <span style="font-size: 60px; text-align: center"
+          >Bienvenido a tu kiosko "Compras Segura"</span
+        >
       </v-card>
-      <v-card>
-        <!-- <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions> -->
+      <v-card class="py-4 d-flex justify-center">
+        <SharedButton
+          type="submit"
+          text="Comprar"
+          :total-width="90"
+          :radius="10"
+          :height="70"
+          :loading="loading"
+          @click="redirect"
+        />
       </v-card>
-    </v-col>
-  </v-row>
+    </div>
+  </div>
 </template>
 
 <script>
+import SharedButton from '@/components/buttons/SharedButton'
 export default {
   name: 'IndexPage',
+  components: {
+    SharedButton,
+  },
+  data() {
+    return {
+      loading: null,
+    }
+  },
+  methods: {
+    redirect() {
+      this.$router.push('/eleccion')
+    },
+  },
 }
 </script>
+<style >
+.contenedor {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+}
+</style>
