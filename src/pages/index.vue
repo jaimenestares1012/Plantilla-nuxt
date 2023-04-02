3a0facb91a6cc3f292864055cf765691fd7ca991
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-04-02 11:51:12 -0500

    comentario

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,11 +2,12 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
+        <!-- <NuxtLogo />
+        <VuetifyLogo /> -->
+        <h1>MOTI PETIOTA</h1>
       </v-card>
       <v-card>
-        <v-card-title class="headline">
+        <!-- <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
         </v-card-title>
         <v-card-text>
@@ -74,7 +75,7 @@
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
+        </v-card-actions> -->
       </v-card>
     </v-col>
   </v-row>
