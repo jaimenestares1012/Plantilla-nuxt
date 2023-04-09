a524d3cd36dd50e971cb87ae14a1c876ff018ca4
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-04-08 21:45:20 -0500

    prueb

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ['~/assets/style/own.css'],
+  css: ['~/assets/own.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~plugins/store-helpers','~/plugins/vuelidate'],
