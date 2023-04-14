26b6e6f783036ae1fb684b382c01c5a4e96d4233
jaimenestares1012 <jaimenestares1012@gmail.com>
2023-04-14 15:09:55 -0500

    commit

--- src/pages/carrito-resumen.vue
+++ src/pages/carrito-resumen.vue
@@ -277,17 +277,8 @@ export default {
               })
               .then(() => {
                 function cutPaper() {
-                  let config = qz.configs.create('BIXOLON BK3-3')
                   const bytes = new Uint8Array([0x1d, 0x56, 0x41, 0x10])
-                  return qz.printRaw(config, bytes)
-                }
-                return cutPaper()
-              })
-              .then(() => {
-                function cutPaper() {
-                  let config = qz.configs.create('BIXOLON BK3-3')
-                  const bytes = new Uint8Array([0x1d, 0x56, 0x41, 0x10])
-                  return qz.print(config, bytes)
+                  return qz.print(bytes)
                 }
                 return cutPaper()
               })
